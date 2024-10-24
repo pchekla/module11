05c0d605db1da77e3a2d2c9033d79912b8e80dad
v.sedlachek <v.sedlachek@dodobrands.io>
2024-10-24 07:11:29 +0300

    adding telegram bot

---
+++ TelegramBot/bot.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TelegramBot;
+
+class Bot
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("This is a Telegram Bot");
+    }
+}
